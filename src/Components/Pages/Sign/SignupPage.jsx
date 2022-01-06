59a73d8fdc97d02264a334d4d09241d493b1cada
Raduev <dominator95rrr@gmail.com>
2022-01-06 14:16:47 +0300

    fix

--- src/Components/Pages/Sign/SignupPage.jsx
+++ src/Components/Pages/Sign/SignupPage.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createUser } from "../../../redux/reducerSign";
 
 const SignupPage = () => {
-    return (
-        <div>
-            регистрация
-        </div>
-    )
-}
+    const dispatch = useDispatch()
+    const [login, setLogin] = useState("")
+    const [password, setPassword] = useState("")
+    const signingUp = useSelector(state => state.application.signingUp)
+    const error = useSelector(state => state.application.error)
 
-export default SignupPage
+    const handleChangeLogin = (e) => {
+        setLogin(e.target.value)
+    }
+
+    const handleChangePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        dispatch(createUser(login, password))
+    }
+
+
+  return (
+    <div>
+        {error}
+      <div>
+        <input
+          type="text"
+          placeholder="type login"
+          value={login}
+          onChange={(e) => handleChangeLogin(e)}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="type password"
+          value={password}
+          onChange={(e) => handleChangePassword(e)}
+        />
+      </div>
+      <button onClick={handleSubmit} disabled={signingUp}>Зарегистрироваться</button>
+    </div>
+  );
+};
+
+export default SignupPage;
--- src/redux/reducerSign.js
+++ src/redux/reducerSign.js
@@ -1,13 +1,34 @@
 const initialStateSign = {
     signingUp: false,
     error: null,
-    token: null,
-    
+
 }
 
 export default function application(state = initialStateSign, action) {
     switch(action.type) {
         default:
             return state
+    } 
+}
+
+export const createUser = (login, password) => {
+    return async dispatch => {
+        dispatch({type: "application/signup/pending"})
+
+        const responce = await fetch("http://localhost:4000/users", {
+            method: "POST",
+            body: JSON.stringify({login, password}),
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+
+        const json = await responce.json()
+
+        if(json.error) {
+            dispatch({type: "application/signup/rejected", error: json.error})
+        } else {
+            dispatch({type: "application/signup/fulfiled", payload: json})
+        }
     } 
 }
