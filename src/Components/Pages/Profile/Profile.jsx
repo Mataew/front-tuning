14de5d02df67c42ee97fe8fce789ae21d0f82876
Husein <ahmadov.m48@gmail.com>
2022-01-06 17:22:37 +0300

    добавил файл reducerProfile

--- src/Components/Pages/Profile/Profile.jsx
+++ src/Components/Pages/Profile/Profile.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './Profile.css'
 
 const Profile = () => {
+
+    
+
     return (
         <div className='mainProfile'>
             <div className="sidebar">
@@ -10,12 +13,13 @@ const Profile = () => {
                 <span>  Фамилия</span>
                 <h4>Номер телефона:</h4>
                 <span>012784</span>
+                <button>Выйти</button>
             </div>
             <div className="cart">
                 <h1>Мои заказы</h1>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default Profile;
