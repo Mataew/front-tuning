a00ad64e197dbed9d2b58bfc7c80ca9857172ba5
Umar <mister.umars@mail.ru>
2022-01-06 11:10:37 +0300

    app.js настроил переходы

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from './Components/Layout/Footer/Footer';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Components/Layout/Layout';
 import Service from './Components/Pages/Service';
+import Contacts from './Components/Pages/contacts/contacts';
+import Company from './Components/Pages/company/company';
 
 const App = () => {
   return (
@@ -13,7 +15,7 @@ const App = () => {
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<Main />} />
           <Route path='/services' element={<Service />}/>
-          {/*<Route path='/company' element={<Company />}/>*/}
+          <Route path='/company' element={<Company />}/>
           <Route path='/contacts' element={<Contacts />}/>
         </Route>
       </Routes>
