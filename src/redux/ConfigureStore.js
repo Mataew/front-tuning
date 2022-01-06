46bece13e4016f42b7c1d95c2546868f21809a67
Umar <mister.umars@mail.ru>
2022-01-06 11:50:01 +0300

    настроил конфигурстор

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,4 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 
@@ -6,6 +7,6 @@ const combineReducer = combineReducers({
 
 })
 
-const store = createStore(combineReducer, applyMiddleware(thunk));
+const store = createStore(combineReducer, composeWithDevTools( applyMiddleware(thunk) ));
 
 export default store;
