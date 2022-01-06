a0dd2045baf693efe259006a2e239105ddfca665
Mamdi <masuev12@gmail.com>
2022-01-06 13:27:32 +0300

    service page

--- src/Components/Pages/Service/Service.jsx
+++ src/Components/Pages/Service/Service.jsx
@@ -1,12 +1,27 @@
-import './Service.css'
-import React from 'react'
+import "./Service.css";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Service = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const services = useSelector((state) => state.service);
+  const dispatch = useDispatch();
+  return (
+    <div className="sevice_container">
+      <h1>Список наших услуг</h1>
+      {/* {services.map((item) => {
+        return (
+          <div className="srvice_services">
+            <div className="service_cart">
+              <img src={item.img} alt="lol"></img>
+              <div className="service_name"></div>
+              {item.name}
+              <button>Добавить услугу</button>
+            </div>
+          </div>
+        );
+      })} */}
+    </div>
+  );
+};
 
-export default Service
+export default Service;
