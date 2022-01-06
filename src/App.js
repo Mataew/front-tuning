b8ecc9f38dea4fd5520ecba1ae8d6cf5b5f3c101
Umar <mister.umars@mail.ru>
2022-01-06 11:20:09 +0300

    поменял кнопку на линк и другие апдейты

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Main from './Components/Pages/main';
 import Footer from './Components/Layout/Footer/Footer';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Components/Layout/Layout';
-import Service from './Components/Pages/Service';
 import Contacts from './Components/Pages/contacts/contacts';
 import Company from './Components/Pages/company/company';
+import SigninPage from './Components/Pages/Sign/SigninPage';
 
 const App = () => {
   return (
@@ -14,9 +14,10 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<Main />} />
-          <Route path='/services' element={<Service />}/>
+          {/*<Route path='/services' element={<Service />}/>*/}
           <Route path='/company' element={<Company />}/>
           <Route path='/contacts' element={<Contacts />}/>
+          <Route path='/signIn' element={<SigninPage />}/>
         </Route>
       </Routes>
     </div>
