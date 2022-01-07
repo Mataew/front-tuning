cad070b32b65c81341efcdb9ea59bc986f833389
Raduev <dominator95rrr@gmail.com>
2022-01-07 09:14:44 +0300

    Completed

--- src/Components/Pages/Sign/SigninPage.jsx
+++ src/Components/Pages/Sign/SigninPage.jsx
@@ -1,12 +1,100 @@
 import React from "react";
 import SignupPage from "./SignupPage";
 import { Link } from "react-router-dom";
-import "./Signin.css"
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "../../../redux/reducerSign";
+import "./Signin.css";
 
 const SigninPage = () => {
+  const dispatch = useDispatch();
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [blurLogin, setBlurLogin] = useState(true);
+  const [blurPassword, setBlurPassword] = useState(true);
+  const [complete, setComplete] = useState("");
+  const signingIn = useSelector((state) => state.application.signingIn);
+  const error = useSelector((state) => state.application.error);
+
+  const handleChangeLogin = (e) => {
+    setLogin(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setLogin("");
+    setPassword("");
+    setComplete(true);
+    dispatch(auth(login, password));
+  };
+
+  const handleBlurLogin = () => {
+    login === "" ? setBlurLogin(false) : setBlurLogin(true);
+  };
+
+  const handleBlurPassword = () => {
+    password === "" ? setBlurPassword(false) : setBlurPassword(true);
+  };
+
   return (
-    <div className="SigninBlock">
-        <Link to="/signUp">Регистрация</Link>
+    <div className="SignUpMain">
+      {error}
+      <div className="SignWindow">
+        <div className="SignUpBlock">
+          <input
+            className="loginINP"
+            onBlur={handleBlurLogin}
+            type="text"
+            placeholder="Введите логин"
+            value={login}
+            onChange={(e) => handleChangeLogin(e)}
+          />
+          {!blurLogin ? (
+            <div className="empty">
+              <span>Поле ввода не должно быть пустым</span>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="SignUpBlock">
+          <input
+            onBlur={handleBlurPassword}
+            className="passwordINP"
+            type="password"
+            placeholder="Введите пароль"
+            value={password}
+            onChange={(e) => handleChangePassword(e)}
+          />
+          {!blurPassword ? (
+            <div className="empty">
+              <span>Поле ввода не должно быть пустым</span>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+        <button
+          className="RegisterBTN"
+          onClick={handleSubmit}
+          disabled={!login || !password || signingIn}
+        >
+          Войти
+        </button>
+        <div className="NoEmpty">
+          {complete === true ? "Вы успешно авторизовались" : ""}
+        </div>
+        <div className="SigninBlock">
+          <span className="textOne">Не зарегистрированы ?</span>
+        <Link className="Link" to="/signUp">Зарегистрироваться</Link>
+      </div>
+      <div className="MainLinkBlock">
+      <Link className="LinkMain" to="/"><button className="getMain">На главную меню</button></Link>
+      </div>
+      </div>
     </div>
   );
 };
--- src/redux/reducerSign.js
+++ src/redux/reducerSign.js
@@ -1,6 +1,8 @@
 const initialStateSign = {
   signingUp: false,
+  signingIn: false,
   error: null,
+  token: localStorage.getItem("token")
 };
 
 export default function application(state = initialStateSign, action) {
@@ -22,18 +24,37 @@ export default function application(state = initialStateSign, action) {
         signingUp: false,
         error: action.error,
       };
+      case "application/signin/pending":
+        return {
+          ...state,
+          signingIn: true,
+          error: null,
+        };
+      case "application/signin/fulfiled":
+        return {
+          ...state,
+          signingIn: false,
+          token: action.payload.token
+        };
+      case "application/signin/rejected":
+        return {
+          ...state,
+          signingIn: false,
+          error: action.error,
+        };
+      
     default:
       return state;
   }
 }
 
-export const createUser = (login, password) => {
+export const createUser = (login, password, /*firstName*/) => {
   return async (dispatch) => {
     dispatch({ type: "application/signup/pending" });
 
     const responce = await fetch("http://localhost:4000/users", {
       method: "POST",
-      body: JSON.stringify({ login, password }),
+      body: JSON.stringify({ login, password, /*firstName*/ }),
       headers: {
         "Content-type": "application/json",
       },
@@ -48,3 +69,26 @@ export const createUser = (login, password) => {
     }
   };
 };
+
+export const auth = (login, password) => {
+  return async dispatch => {
+    dispatch({ type: "application/signin/pending" });
+
+    const responce = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({ login, password }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const json = await responce.json();
+
+    if (json.error) {
+      dispatch({ type: "application/signin/rejected", error: json.e.message });
+    } else {
+      dispatch({ type: "application/signin/fulfiled", payload: json });
+      localStorage.setItem("token", json.token)
+    }
+  }
+}
