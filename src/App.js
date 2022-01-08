f60321cd8b2dd802fd73da1cad773760e6ed51b7
Umar <mister.umars@mail.ru>
2022-01-08 12:28:13 +0300

    исправил роуты с переходом после авторизации

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main from './Components/Pages/main';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { Layout } from './Components/Layout/Layout';
 import Contacts from './Components/Pages/contacts/contacts';
 import Company from './Components/Pages/company/company';
@@ -8,9 +8,29 @@ import SigninPage from './Components/Pages/Sign/SigninPage';
 import Service from './Components/Pages/Service/Service';
 import Profile from './Components/Pages/Profile/Profile';
 import SignupPage from './Components/Pages/Sign/SignupPage';
+import { useSelector } from 'react-redux';
 
 
 const App = () => {
+
+  const token = useSelector(state => state.application.token)
+
+
+  if (!token){
+    return (
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Main />} />
+          <Route path='/services' element={<Service />}/>
+          <Route path='/company' element={<Company />}/>
+          <Route path='/contacts' element={<Contacts />}/>
+          <Route path='/signIn' element={<SigninPage />}/>
+          <Route path='/signUp' element={<SignupPage />}/>
+          <Route path='/profile' element={<Navigate to='/signIn' replace/>}/>
+        </Route>
+      </Routes>
+    )
+  }
   return (
     <div>
       <Routes>
@@ -20,12 +40,13 @@ const App = () => {
           <Route path='/company' element={<Company />}/>
           <Route path='/profile' element={<Profile />}/>
           <Route path='/contacts' element={<Contacts />}/>
-          <Route path='/signIn' element={<SigninPage />}/>
-          <Route path='/signUp' element={<SignupPage />}/>
+          {/*<Route path='/signIn' element={<Navigate to='/' replace/>}/>*/}
         </Route>
       </Routes>
     </div>
   );
+
+
 };
 
 export default App;
