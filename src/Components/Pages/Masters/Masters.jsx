b2aaa90fdf0d0c22ca3b7c2a00f776c6c2f09445
Ibragim <mih230903@gmail.com>
2022-01-10 16:01:51 +0300

    master reducer

--- src/Components/Pages/Masters/Masters.jsx
+++ src/Components/Pages/Masters/Masters.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import "./master.css";
+import { useDispatch, useSelector } from "react-redux";
 
 const Masters = () => {
+  const masters = useSelector((state) => state.masters);
+
+  console.log(masters);
+
   return (
-    <div>
-      
+    <div className="master_main">
+      <h1>Мастера меча онлайн</h1>
+      <h2>Выберите своего мастера</h2>
+      <hr />
+      <div className="master_flex_blocks"></div>
+      {masters.map((item, index) => {
+        return (
+          <div>
+            {item.image}
+            {item.name}
+          </div>
+        )
+      })}
     </div>
   );
 };
